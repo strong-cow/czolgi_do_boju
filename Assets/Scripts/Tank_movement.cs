befedcda2a0f0fc4de2086dcdf11e9fd3976d627
strong-cow <120826906+strong-cow@users.noreply.github.com>
2023-06-08 18:35:35 +0200

    2 players support

--- Assets/Scripts/Tank_movement.cs
+++ Assets/Scripts/Tank_movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Tank_movement : MonoBehaviour
@@ -7,7 +8,8 @@ public class Tank_movement : MonoBehaviour
     public float MaxEnginePower;
     public float MaxSteeringPower;
     private Rigidbody2D myRigidbody;
-
+    public GameObject target;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +19,62 @@ public class Tank_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float power = Input.GetAxisRaw("Vertical");
+        if (target.CompareTag("Player1") == true) { Player1Control(); }
+        if (target.CompareTag("Player2") == true) { Player2Control(); }
+
+    }
+    
+    void Player1Control()
+    {
+
+        float power = 0;
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            power = 1;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            power = -1;
+        }
+
         myRigidbody.AddForce(transform.up * MaxEnginePower * power, ForceMode2D.Force);
 
-        float steering = Input.GetAxisRaw("Horizontal");
+        float steering = 0;
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            steering = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            steering = -1;
+        }
+        myRigidbody.AddTorque(-steering * MaxSteeringPower, ForceMode2D.Force);
+    }
+        
+    void Player2Control()
+    {
+
+        float power = 0;
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            power = 1;
+        }
+        else if (Input.GetKey(KeyCode.S) == true)
+        {
+            power = -1;
+        }
+
+        myRigidbody.AddForce(transform.up * MaxEnginePower * power, ForceMode2D.Force);
+
+        float steering = 0;
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            steering = 1;
+        }
+        else if (Input.GetKey(KeyCode.A) == true)
+        {
+            steering = -1;
+        }
         myRigidbody.AddTorque(-steering * MaxSteeringPower, ForceMode2D.Force);
     }
 }
